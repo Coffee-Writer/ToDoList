5076a9bca172fc996fc3d6324baa599094aad0df
Nathan Ridgeway <ridgewayjkd@gmail.com>
2016-01-19 05:53:06 -0500

    First Commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(function() {
+    
+    //SETUP
+    var $list, $newItemForm, $newItemButton;
+    var item = '';
+    $list = $('ul');
+    $newItemForm = $('#newItemForm');
+    $newItemButton = $('#newItemButton');
+    
+    $('li').hide().each(function(index) {
+        $(this).delay(450 * index).fadeIn(1600);
+    });
+    
+});
